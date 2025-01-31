bdf83558db3e84880d9a008bfe65775ca706355c
unknown <humzaahmad_@hotmail.com>
2025-01-31 14:34:08 +0000

    fixed service issue for films

--- src/main/java/com/example/sakila/services/FilmService.java
+++ src/main/java/com/example/sakila/services/FilmService.java
@@ -7,6 +7,7 @@ import com.example.sakila.repositories.FilmRepository;
 import com.example.sakila.repositories.LanguageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
@@ -14,6 +15,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+@Service
 public class FilmService {
 
     private final FilmRepository filmRepository;
