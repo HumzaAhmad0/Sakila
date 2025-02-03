5dd4e61b810a069198de24fcae1ce50a951ce19d
unknown <humzaahmad_@hotmail.com>
2025-02-03 11:30:11 +0000

    added validation messages for actors

--- src/main/java/com/example/sakila/dto/input/ActorInput.java
+++ src/main/java/com/example/sakila/dto/input/ActorInput.java
@@ -8,12 +8,12 @@ import com.example.sakila.dto.ValidationGroup.*;
 import java.util.List;
 @Getter
 public class ActorInput {
-    @NotNull(groups = {Put.class, Post.class})
-    @Size(min = 1, max = 45)
+    @NotNull(groups = {Put.class, Post.class}, message = "First Name cannot be null")
+    @Size(min = 1, max = 45, message = "First Name must be between 1-45 characters")
     private String firstName;
 
-    @NotNull(groups = {Put.class, Post.class})
-    @Size(min = 1, max = 45)
+    @NotNull(groups = {Put.class, Post.class}, message = "Last Name cannot be null")
+    @Size(min = 1, max = 45, message = "Last Name must be between 1-45 characters")
     private String lastName;
 
     private List<Short> films;
