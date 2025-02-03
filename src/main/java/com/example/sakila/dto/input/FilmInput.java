657f8e0801e1fd9fa91ef87049b8b55a9beb5992
unknown <humzaahmad_@hotmail.com>
2025-02-03 15:09:41 +0000

    added validation for film input

--- src/main/java/com/example/sakila/dto/input/FilmInput.java
+++ src/main/java/com/example/sakila/dto/input/FilmInput.java
@@ -17,16 +17,17 @@ public class FilmInput {
     private Year releaseYear;
 
     @NotNull(groups = {ValidationGroup.Put.class, ValidationGroup.Post.class}, message = "Language cannot be null")
-    //@Positive
     @Min(value = 1, message = "Movie length cannot be below 1")
     private Byte language;
+    // range from 1 to  32767
+    @Min(value = 1, message = "Movie length can not be below 1")
+    @Max(value = 32767, message = "Movie length can not be above 32767")
     private Short movieLength;
 
     @Pattern(regexp = "^(G|PG|PG-13|R|NC-17)$", message = "Rating can only be: 'G, PG, PG-13, R, NC-17'")
     private String rating;
     private List<Short> actors;
 
-    //maybe have a validation?
     private List<Byte> genre;
 
     @DecimalMin(value = "0.00", inclusive = true, message = "Score cannot be below 0.00")
@@ -37,11 +38,11 @@ public class FilmInput {
     @NotNull(groups = {ValidationGroup.Put.class, ValidationGroup.Post.class})
     @DecimalMin(value = "0.00", inclusive = true, message = "Rental Rate cannot be below 0.00")
     @DecimalMax(value = "99.99", inclusive = true, message = "Rental Rate cannot be above 0.00")
-    // issue with: out of range above 99.99
     @Digits(integer = 2, fraction = 2, message = "Rental rate can only be 3 digits before the decimal and 2 digits after e.g XXX.YY")
     private Float rentalRate;
 
     @NotNull(groups = {ValidationGroup.Put.class, ValidationGroup.Post.class}, message = "Rental Duration cannot be null")
     @Min(value = 1, message = "Rental Duration cannot be below 1")
+    @Max(value = 255, message = "rental duration cannot exceed 255")
     private Byte rentalDuration;
 }
